7b1cf19547708987c41655e00db72248fdfa34d4
devadrsilva <adriano.mssilva1@gmail.com>
2023-08-03 17:24:46 -0300

    CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
 # modo jr de fazer
 root 'home#index'
+
+# CRUD na unha
+get '/vehicles', controller: 'vehicles', action: 'index'
+get '/vehicles/new', controller: 'vehicles', action: 'new'
+post '/vehicles', controller: 'vehicles', action: 'create'
+get '/vehicles/:id', controller: 'vehicles', action: 'show'
+get '/vehicles/:id/edit', controller: 'vehicles', action: 'edit'
+put '/vehicles/:id', controller: 'vehicles', action: 'update'
+patch '/vehicles/:id', controller: 'vehicles', action: ''
+delete '/vehicles/:id', controller: 'vehicles', action: ''
 end
