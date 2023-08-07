06b3eb7fd682590f704ab54e3c9c82ba2560a089
devadrsilva <adriano.mssilva1@gmail.com>
2023-08-07 20:31:56 -0300

    parei em update

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -12,8 +12,34 @@ class VehiclesController < ApplicationController
    def create
       @vehicle = Vehicle.new(vehicle_params)
       
+      if @vehicle.save
+         redirect_to  vehicle_path(@vehicle)
+      else
+         render :new
+      end
    end
 
+   def show
+      @vehicle = Vehicle.find(params[:id])
+      
+   end
+
+   def edit
+      @vehicle = Vehicle.find(params[:id])
+
+   end
+
+   def update
+      @vehicle = Vehicle.find(params[:id])
+      
+      if @vehicle.update(vehicle_params)
+         redirect_to  vehicle_path(@vehicle)
+      else
+         render :edit
+      end
+   end
+   
+   
    private
 
    def vehicle_params
